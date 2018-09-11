7eae954e6a60856965d8ce29f8558d6cdcccd004
Atticus Zambrana <atticus.zambrana@angusbeefgaming.net>
2018-09-11 16:37:12 -0700

    Added getPlayerCount() to ServerManager class

--- src/net/angusbeefgaming/api/Main.java
+++ src/net/angusbeefgaming/api/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import net.angusbeefgaming.api.bungee.NetworkManager;
 import net.angusbeefgaming.api.bungee.PlayerManager;
+import net.angusbeefgaming.api.bungee.ServerManager;
 
 public class Main extends JavaPlugin {
 	static Main instance;
@@ -21,6 +22,7 @@ public class Main extends JavaPlugin {
 	    // For Recieving Messages
 	    getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new NetworkManager());
 	    getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new PlayerManager());
+	    getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new ServerManager());
 	    
 	    // Start the API
 	}
--- src/net/angusbeefgaming/api/bungee/ServerManager.java
+++ src/net/angusbeefgaming/api/bungee/ServerManager.java
@@ -1,9 +1,55 @@
 package net.angusbeefgaming.api.bungee;
 
-public class ServerManager {
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import com.google.common.collect.Iterables;
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+public class ServerManager implements PluginMessageListener {
+	
+	static Map<String, Integer> count = new HashMap<String, Integer>();
+	static Map<String, Integer> localStorage = new HashMap<String, Integer>();
 	/*
 	 * Server Manager Class
 	 * Written by Atticus Zambrana
 	 */
+	
+	/**
+	 * Get the number of players on a server
+	 * @param server
+	 * @return
+	 */
+	public int getPlayerCount(String server) {
+		ByteArrayDataOutput out = ByteStreams.newDataOutput();
+		out.writeUTF("PlayerCount");
+		out.writeUTF(server);
+		Player sender = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
+		if(count != null) {
+			localStorage = count;
+			count = null;
+			return localStorage.get(server);
+		}
+		else {
+			return getPlayerCount(server);
+		}
+	}
+	
+	@Override
+	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		if (!channel.equals("BungeeCord")) return;
+		
+	      ByteArrayDataInput in = ByteStreams.newDataInput(message);
+	      String subchannel = in.readUTF();
+	      if (subchannel.equals("PlayerCount")) {
+	    	  count.put(in.readUTF(), in.readInt());
+	      }
+	}
 
 }
